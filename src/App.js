a9625c634e6b0fb4de68c688377118db110fddc2
Kazi Md. Mahidul Islam <mahidul.cse.du@gmail.com>
2021-06-18 00:20:28 +0600

    The net ninja Lecture #4

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title  =  'React native';
+  const msg = 'We are ready to learn';
+  const like = 50;
+  const msg2 = 'like avobe 50'
+  var fb = "https://facebook.com";
   return (
     <div className="App">
-      <h1>React native start----</h1>
-      <h3>App component</h3>
+      <h1>{title}</h1>
+      <h3>{msg}</h3>
+      <p>{Math.random()*100}</p>
+      <p>{[1,' ',2,' ',3,' ',4]}</p>
+      <a href={fb}>facebook</a>
     </div>
   );
 }
