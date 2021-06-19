4388bddc900b68c4fc52e5a13d9efa0a802e1145
Kazi Md. Mahidul Islam <mahidul.cse.du@gmail.com>
2021-06-20 01:06:05 +0600

    lecture 7 and 8 : how to use onClick method, how use useState of react component

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,34 @@
+import { useState } from "react";
+var val = 0;
+
 const Home = () => {
+    const [preValue, updateValue] = useState(0);
+    
+    function handleClick(e){
+        console.log('This is handleClick' , e.target);
+    }
+
+    const handleParameter = (name,e) => {
+        console.log('handle click with parameter ' , name , e.detail);
+    }
+
+function changeState()
+{
+    val= val+1;
+    console.log(val);
+    updateValue(val);
+}
+
     return ( 
         <div className="home">
-            <h1>This is home</h1>
+            <h2>This is home</h2>
+            <button onClick = {handleClick}>Click me</button>
+            <button onClick = { (e) => handleParameter('misbah',e)}>Click me again</button>
+
+            <p>{preValue}</p>
+            <button onClick = {changeState}>count your click</button>
+
         </div>
      );
 }
- 
 export default Home;
