f300f5a9204f878dd81d88efcac222b831214ec2
Kazi Md. Mahidul Islam <mahidul.cse.du@gmail.com>
2021-06-21 01:48:18 +0600

    learn how to use props, create template and re-use it

--- src/Home.js
+++ src/Home.js
@@ -21,12 +21,18 @@ function changeState()
 
     return ( 
         <div className="home">
-            <h2>This is home</h2>
-            <button onClick = {handleClick}>Click me</button>
-            <button onClick = { (e) => handleParameter('misbah',e)}>Click me again</button>
+            <h2 style ={{textEmphasisColor: "green"}}>Welcome to my palace</h2><br />
+            <label htmlFor="username" style ={{width: "200px",height: "15px", margin:"0px 5px 5px 0px"}}>User Name: </label>
+            <input type="text" /><br /><br />
+            
+            <label htmlFor="password" style ={{width: "200px",height: "15px"}}>Password : </label>
+            <input type="password" style={{marginLeft: "10px"}}/><br />
+            <button onClick = {handleClick}>Sign in</button><br /><b></b>
+             <label htmlFor="msg">Please Sign up here  </label>
+            <button onClick = { (e) => handleParameter('misbah',e)}>Sign up</button>
 
             <p>{preValue}</p>
-            <button onClick = {changeState}>count your click</button>
+            <button onClick = {changeState}>Click here show book list:</button>
         </div>
 
         
