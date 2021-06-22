42120d1e123cc5aadc20fdfa281f699d2c91d713
mahidulmisbah <mahidul.cse.du@gmail.com>
2021-06-22 16:01:08 +0600

    template re-using and delete somthing from web page

--- src/DynamicTable.js
+++ src/DynamicTable.js
@@ -2,22 +2,22 @@
 // Learn how to use props
 
 import { useState } from "react"; 
-import BookList from "./bookList";  
+import BookListTemplate from "./bookListTemplate";  
 
 const DynamicTable = () => {
-    const [blogs] = useState([
+    const [blogs,newList] = useState([
         {title: 'Mara khao', author: 'Misbah',publishYear: 2019, id : '1'},
         {title: 'khaite khaite shes', author: 'Kundu',publishYear: 2020, id : '2'},
         {title: 'ami ar partesina', author: 'Pijush',publishYear: 2021, id : '3'}
     ]);
-
-
-
+    const DeleteRow = (id) => {
+        const setNewList = blogs.filter(blog => blog.id !==id);
+        newList(setNewList);
+    }
     return (
+  
         <div className="tableDiv">
-            <div className="blog-preview" >
-                <BookList blogs = {blogs} message  = "See the book list" />
-            </div>
+             <BookListTemplate blogs = {blogs} message  = "See the book list" handleDelete={DeleteRow} />
         </div>
      );
 }
