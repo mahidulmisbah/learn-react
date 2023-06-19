0a3f1fcf8739c038ab1bdb1bf58e4e5982a34d10
mahidulmisbah <mahidul.cse.du@gmail.com>
2023-06-19 21:44:56 +0600

    create-react-app initial commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import Navbar from './Navbar';
-import Home from './Home';
-import DynamicTable from './DynamicTable';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
-      </div>
-      <div className="maplearn">
-        <DynamicTable />
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
-export default App; 
+export default App;
